4bd1f7be22e45c4310f4cd953c397fdd1999f109
main12sani <main12sani@gmail.com>
2021-09-11 19:13:15 +0600

    2 page design & responsive

---
+++ quotation.php
@@ -0,0 +1,166 @@
+<?php include 'inc/headers/header-link.php'; ?>
+<?php include 'inc/headers/header.php'; ?>
+
+<!-- ===================  bradecrumb part start ==================== -->
+<nav aria-label="breadcrumb" class="ic-pt-30 ic-pb-30 gray-light-bg">
+    <div class="container">
+        <ol class="breadcrumb bg-transparent p-0 m-0">
+            <li class="breadcrumb-item main-color"><a href="index.php">Home</a></li>
+            <li class="breadcrumb-item"><a href="#">Quotation</a></li>
+        </ol>
+    </div>
+</nav>
+<!-- ===================  bradecrumb part start ==================== -->
+
+<!-- ===================  service part start ==================== -->
+<section class="ic-service-part ic-pb-60 ic-pt-60">
+    <div class="container">
+        <div class="ms-quotation-heads">
+            <h3>Send Your image for Quotation</h3>
+            <p><b>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, ullam.</b></p>
+        </div>
+        <div class="ic-quotation">
+            <form action="#" class="ic-pt-20 ic-pb-20">
+                <hr class="colorgraph">
+                <div class="ic-form-group">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="ic-checkbox-heads">
+                                <div class="form-group form-check">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                </div>
+                                <div class="form-group form-check">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck2">
+                                    <label class="form-check-label" for="exampleCheck2">Check me out</label>
+                                </div>
+                                <div class="form-group form-check">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck3">
+                                    <label class="form-check-label" for="exampleCheck3">Check me out</label>
+                                </div>
+                                <div class="form-group form-check">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck4">
+                                    <label class="form-check-label" for="exampleCheck4">Check me out</label>
+                                </div>
+                                <div class="form-group form-check">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck5">
+                                    <label class="form-check-label" for="exampleCheck5">Check me out</label>
+                                </div>
+                                <div class="form-group form-check">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck6">
+                                    <label class="form-check-label" for="exampleCheck6">Check me out</label>
+                                </div>
+                                <div class="form-group form-check">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck7">
+                                    <label class="form-check-label" for="exampleCheck7">Check me out</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="ic-checkbox-heads">
+                                <div class="form-group form-check">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                </div>
+                                <div class="form-group form-check">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck2">
+                                    <label class="form-check-label" for="exampleCheck2">Check me out</label>
+                                </div>
+                                <div class="form-group form-check">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck3">
+                                    <label class="form-check-label" for="exampleCheck3">Check me out</label>
+                                </div>
+                                <div class="form-group form-check">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck4">
+                                    <label class="form-check-label" for="exampleCheck4">Check me out</label>
+                                </div>
+                                <div class="form-group form-check">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck5">
+                                    <label class="form-check-label" for="exampleCheck5">Check me out</label>
+                                </div>
+                                <div class="form-group form-check">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck6">
+                                    <label class="form-check-label" for="exampleCheck6">Check me out</label>
+                                </div>
+                                <div class="form-group form-check">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck7">
+                                    <label class="form-check-label" for="exampleCheck7">Check me out</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="ic-input-quotations">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="form-group">
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <label class="input-group-text" for="inputGroupSelect01"><i class="icofont-image"></i></label>
+                                    </div>
+                                    <select class="custom-select" id="inputGroupSelect01">
+                                        <option selected>Choose...</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="icofont-plus"></i></span>
+                                    </div>
+                                    <input type="text" class="custom-select bg-transparent-input" placeholder="Quantity">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <label class="input-group-text" for="inputGroupSelect02"><i class="icofont-clock-time"></i></label>
+                                    </div>
+                                    <select class="custom-select" id="inputGroupSelect02">
+                                        <option selected>Choose...</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="form-group">
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="icofont-pencil-alt-1"></i></span>
+                                    </div>
+                                    <textarea class="custom-select bg-transparent-input" placeholder="write"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck7">
+                        <label class="form-check-label" for="exampleCheck7">I am not a robot</label>
+                    </div>
+                </div>
+                <hr class="colorgraph">
+                <button class="ic-btn">Next</button>
+            </form>
+        </div>
+    </div>
+</section>
+<!-- ===================  service part End ==================== -->
+
+
+
+<!-- Scroll Top Area Start -->
+<!-- <a href="#" class="ic-scroll-top"><i class="flaticon-upload"></i></a> -->
+<!-- Scroll Top Area Start -->
+
+
+<?php include 'inc/footers/footer.php'; ?>
---
+++ service.php
@@ -0,0 +1,213 @@
+<?php include 'inc/headers/header-link.php'; ?>
+<?php include 'inc/headers/header.php'; ?>
+
+<!-- ===================  bradecrumb part start ==================== -->
+<nav aria-label="breadcrumb" class="ic-pt-30 ic-pb-30 gray-light-bg">
+    <div class="container">
+        <ol class="breadcrumb bg-transparent p-0 m-0">
+            <li class="breadcrumb-item main-color"><a href="index.php">Home</a></li>
+            <li class="breadcrumb-item"><a href="#">Service</a></li>
+        </ol>
+    </div>
+</nav>
+<!-- ===================  bradecrumb part start ==================== -->
+
+<!-- ===================  service part start ==================== -->
+<section class="ic-service-part ic-pb-60">
+    <div class="container">
+        <div class="row ic-mt-60 align-items-center">
+            <div class="col-lg-5 col-md-6">
+                <div class="after-before-img">
+                    <div class="beforeafterautoslide">
+                        <div data-type="data-type-image">
+                            <div data-type="before"><img src="assets/images/clipping-Path.jpg" alt=""></div>
+                            <div data-type="after"><img src="assets/images/clipping-Path22.jpg" alt=""></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6  offset-lg-1">
+                <div class="cliping-path-heading">
+                    <h3>Ecommerce Image</h3>
+                    <h6><b>Make product photos that generate sells</b></h6>
+                    <p>Our eCommerce product photo editing allows you to generate better product images to increase the sales. Get the best product retouching along with some advanced features for your product image.</p>
+                    <ul class="ms-check">
+                        <li>
+                            <p> <img src="assets/images/check.svg" alt=""> Understand Product Framing</p>
+                        </li>
+                        <li>
+                            <p> <img src="assets/images/check.svg" alt=""> Bulk Project Handling Experience</p>
+                        </li>
+                        <li>
+                            <p> <img src="assets/images/check.svg" alt=""> Providing Accurate White Background</p>
+                        </li>
+                        <li>
+                            <p> <img src="assets/images/check.svg" alt=""> Following Online Image Requirements</p>
+                        </li>
+                    </ul>
+                    <a href="#" class="ic-btn">Try Use Free</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ===================  service part End ==================== -->
+
+<!-- ===================  get connect with us part Start ==================== -->
+<section class="ic-get-connect-main-head ic-pt-80 ic-pb-80 gray-light-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 m-auto">
+                <div class="section-heading text-center">
+                    <h2>get connected with us</h2>
+                    <p>Nothing can beat the combination of professional photo editing services at an affordable price as we offer. So get the highest values with the services you are looking for.</p>
+                </div>
+            </div>
+        </div>
+        <div class="ic-logo-show ic-pt-60 ic-pb-60">
+            <a href="#"><img src="assets/images/Houzz-01.png" class="img-fluid" alt=""></a>
+            <a href="#"><img src="assets/images/Zalando-01.png" class="img-fluid" alt=""></a>
+            <a href="#"><img src="assets/images/Shopify-01.png" class="img-fluid" alt=""></a>
+            <a href="#"><img src="assets/images/Media-01.png" class="img-fluid" alt=""></a>
+        </div>
+    </div>
+</section>
+<!-- ===================  get connect with us part End ==================== -->
+
+<!-- ===================  Shadow creation sample part Start ==================== -->
+<section class="ic-get-connect-main-head ic-pt-60 ic-pb-60">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 m-auto">
+                <div class="section-heading text-center">
+                    <h2>Shadow creation sample</h2>
+                    <p>Nothing can beat the combination of professional photo editing services at an affordable price as we offer. So get the highest values with the services you are looking for.</p>
+                </div>
+            </div>
+        </div>
+        <div class="ic-shadow-wellpapper ic-pt-60">
+            <div class="row">
+                <div class="col-lg-4 col-md-4">
+                    <div class="ic-img-shadow-same-height">
+                        <a class="venobox" data-gall="gallery01" href="assets/images/image-3___.png"> <img src="assets/images/image-3___.png" class="img-fluid w-100" alt=""></a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4">
+                    <div class="ic-img-shadow">
+                        <a class="venobox" data-gall="gallery01" href="assets/images/image-down-1.png"><img src=" assets/images/image-down-1.png" class="img-fluid w-100" alt=""></a>
+                    </div>
+                    <div class="ic-img-shadow">
+                        <a class="venobox" data-gall="gallery01" href="assets/images/image-up-2.pn"><img src="assets/images/image-up-2.png" class="img-fluid w-100" alt=""></a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4">
+                    <div class="ic-img-shadow-same-height">
+                        <a class="venobox" data-gall="gallery01" href="assets/images/image-3___.png"><img src="assets/images/image-3___.png" class="img-fluid w-100" alt=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a href="#" class="more-protfolio ic-mt-40">Check out more
+            <svg id="Layer_1_1_" style="enable-background:new 0 0 16 16;" version="1.1" viewBox="0 0 16 16" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <path d="M8,0C3.582,0,0,3.582,0,8s3.582,8,8,8s8-3.582,8-8S12.418,0,8,0z M6.354,13.354l-0.707-0.707L10.293,8L5.646,3.354  l0.707-0.707L11.707,8L6.354,13.354z"></path>
+            </svg>
+            from protfolio page</a>
+    </div>
+</section>
+<!-- ===================  Shadow creation sample part End ==================== -->
+
+<!-- ===================  Shadow creation sample part Start ==================== -->
+<section class="ic-get-connect-main-head ic-pt-60 ic-pb-60 gray-light-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="section-heading text-center">
+                    <h2>Benefits Of Shadow Creation Service</h2>
+                    <p>Lighting and shadow are two of the major elements of a photo. Without proper lighting, you can't capture better images, and without shadow, you can't give depth to your image and make it look realistic. So the benefits of shadow-making service aren't any less than the photography lighting. Let's dive deep into some of the beneficial features that the photoshop shadow effect service holds.</p>
+                </div>
+            </div>
+        </div>
+        <div class="ic-shadow-wellpapper ic-pt-60">
+            <div class="ic-paragraph-block">
+                <h6>Premium Quality Image</h6>
+                <p>There is no space for any argument that the shadow effect helps to make premium quality images. Lack of shadow or a terrible shadow can destroy your image and ruin all the impressions. So shadow will make your image unforgettable if you apply it perfectly.</p>
+            </div>
+            <div class="ic-paragraph-block">
+                <h6>Natural Looking Image</h6>
+                <p>In eCommerce stores, people buy a product only if its appearance convinces them, and the formation of a product online should be natural. Without shadow, products look floating, and it creates an unrealistic look. So shadow enables you to make a natural-looking image and also increases its charm.</p>
+            </div>
+            <div class="ic-paragraph-block">
+                <h6>Hiding Distractions</h6>
+                <p>Sometimes you get the perfect shadow, but images get distractions in the background. So when experts erase the background, the shadow also gets cropped along with it. It is better to add a solid background before creating any shadow. After adding the solid background, if you add the proper amount of shadow, the image will lose its distractions and be an excellent version.</p>
+            </div>
+            <div class="ic-paragraph-block">
+                <h6>More Appealing</h6>
+                <p>Online products must look appealing to grab consumers' attention. Using shadow, you can improve the conversion rate by making your product images more attractive.</p>
+            </div>
+            <div class="ic-paragraph-block">
+                <h6>Attention Grabber</h6>
+                <p>Detailed and accurate shadow creation in photoshop can be the game-changer for your business. So experts with an eye for attention to detail and knowledge of using correct angles while making shadows can make the ultimate attention grabber product image.</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ===================  Shadow creation sample part End ==================== -->
+<!-- ===================  Shadow creation sample part Start ==================== -->
+<section class="ic-get-connect-main-head ic-pt-60 ic-pb-60 ">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="section-heading text-center">
+                    <h2>When To Use Photoshop Drop Shadow Services</h2>
+                    <p>Lighting and shadow are two of the major elements of a photo. Without proper lighting, you can't capture better images, and without shadow, you can't give depth to your image and make it look realistic. So the benefits of shadow-making service aren't any less than the photography lighting. Let's dive deep into some of the beneficial features that the photoshop shadow effect service holds.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ===================  Shadow creation sample part End ==================== -->
+<!-- ===================  Shadow creation sample part Start ==================== -->
+<section class="ic-get-connect-main-head ic-pt-60 ic-pb-60 gray-light-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="section-heading text-center">
+                    <h2>Why Is Our Photoshop Shadow Creation Very Impressive?</h2>
+                    <p>The best combination of quality editing, fastest turnaround time, safety, security, and client-friendly solution for shadow creation in photoshop is the ultimate achievement of our years of working experience. Meeting your marketing goal wasn't as easy as it is now with our shadow-making service. Features that allow us to guarantee our service is given below:</p>
+                </div>
+            </div>
+        </div>
+        <div class="ic-shadow-wellpapper ic-pt-60">
+            <div class="ic-paragraph-block">
+                <h6>Customizable Shadow Making Service</h6>
+                <p>You can customize your photoshop shadow effect according to your budget and taste. Our concern is to provide the best possible image editing service to our clients to get premium images for their marketing.</p>
+            </div>
+            <div class="ic-paragraph-block">
+                <h6>Uninterrupted Assistance</h6>
+                <p>Our customer support department is working 24/7 to provide any guidance regarding your queries. You can send your images to our free trial process to check our quality or just ask your query, and our experts will embrace you in no time.</p>
+            </div>
+            <div class="ic-paragraph-block">
+                <h6>Excellent Quality Service</h6>
+                <p>For each project, we aim to retain the same consistency. We calculate and assess the deliverables of each project. Besides, our quality management professionals will make sure that you are 100% pleased.</p>
+            </div>
+            <div class="ic-paragraph-block">
+                <h6>Safety and Security in Data Transfering</h6>
+                <p>Risking your critical and classified unique images while transferring for outsourcing through an unsecured transferring system isn't a wise decision. At TGL, we have a personalized premium subscription to one of the best online file sharing systems and our FTP system to ensure data transfer.</p>
+            </div>
+            <div class="ic-paragraph-block">
+                <h6>Affordable Pricing</h6>
+                <p>If you have budgetary limits, you should meet us. We have a tailor-made shadow creation service and will amend the project to provide cost-effective services.</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ===================  Shadow creation sample part End ==================== -->
+
+
+
+<!-- Scroll Top Area Start -->
+<!-- <a href="#" class="ic-scroll-top"><i class="flaticon-upload"></i></a> -->
+<!-- Scroll Top Area Start -->
+
+
+<?php include 'inc/footers/footer.php'; ?>
